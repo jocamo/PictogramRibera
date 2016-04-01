8f29fcf68cfbb7142e95992be8595656c796d64e
jocamo <jocamo@campusaula.com>
2016-04-01 12:49:51 +0200

    Initial commit

---
+++ app/src/main/java/com/pymematica/hp/pictogramribera/MainActivity.java
@@ -0,0 +1,19 @@
+package com.pymematica.hp.pictogramribera;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_seis);
+
+    }
+
+
+}
